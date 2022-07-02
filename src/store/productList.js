6b2303a5c8a68fc0522dd498c73d3ac1015e614f
guru6163 <gprasath20@gmail.com>
2022-07-02 15:26:27 +0530

    Stored All Data in Redux || Setup Env FIle || API changed

--- src/store/productList.js
+++ src/store/productList.js
@@ -1,17 +1,78 @@
+const initialState = {
+    isLoading: false,
+    loadError: null,
+    data: []
+};
 
-const initialState = []
+const FETCH_PRODUCT_LIST_INIT = "FETCH_PRODUCT_LIST_INIT";
+const FETCH_PRODUCT_LIST_DONE = "FETCH_PRODUCT_LIST_DONE";
+const FETCH_PRODUCT_LIST_ERROR = "FETCH_PRODUCT_LIST_ERROR";
 
-const loadProductList=(categoryName)=>{
-    return ()=>{
-        
-    }
-}
+const fetchProductListDone = (productList) => ({
+    type: FETCH_PRODUCT_LIST_DONE,
+    payload: productList
+});
+
+const fetchProductListError = (error) => ({
+    type: FETCH_PRODUCT_LIST_ERROR,
+    payload: error
+});
+
+const fetchProductListInit = () => ({
+    type: FETCH_PRODUCT_LIST_INIT
+});
+
+const loadProductList = (categoryName) => {
+    return async (dispatch, getState) => {
+        const { isLoading } = getState().productList;
+
+        if (isLoading) return;
+
+        dispatch(fetchProductListInit());
+
+        try {
+            const response = await fetch(
+                `${process.env.REACT_APP_API_URL}/${categoryName}`
+            );
+            const json = await response.json();
+            dispatch(fetchProductListDone(json));
+        } catch (e) {
+            dispatch(fetchProductListError(e));
+        }
+    };
+};
 
 const productReducer = (state = initialState, action) => {
     switch (action.type) {
+        case FETCH_PRODUCT_LIST_INIT: {
+            return {
+                ...state,
+                isLoading: true,
+                loadError: null,
+                data: []
+            };
+        }
+        case FETCH_PRODUCT_LIST_DONE: {
+            return {
+                ...state,
+                isLoading: false,
+                loadError: null,
+                data: action.payload
+            };
+        }
+        case FETCH_PRODUCT_LIST_ERROR: {
+            return {
+                ...state,
+                isLoading: false,
+                loadError: action.payload,
+                data: []
+            };
+        }
         default:
-            return state
+            return state;
     }
-}
+};
 
-export default productReducer
+export { loadProductList };
+
+export default productReducer;
